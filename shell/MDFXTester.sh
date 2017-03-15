855d9694a7c8db3c61b3c497f4c9032e37cb799f
Justin C. Bagley <justincbagley@users.noreply.github.com>
2017-03-15 14:09:22 -0500

    Update MDFXTester.sh

--- shell/MDFXTester.sh
+++ shell/MDFXTester.sh
@@ -9,13 +9,18 @@ echo "DO GRAPHICS CHECK ON BASIC SCATTERPLOTS FROM R; IF FAILED, DO fetchTaxonLa
 echo "AND COMPLETELY RERUN MissingDataFX SCRIPT. "
 
 if [[ -n $(find . -name "basic_scatterplots.pdf" -type f) ]] || [[ -n $(find ../R_results/ -name "basic_scatterplots.pdf" -type f) ]]; then
-	echo "     Passed graphics check. Moving on... "
+	echo "     Passed graphics check. Moving on... 
+"
 else
 	echo "     WARNING! FAILED graphics check. Running fetchTaxonLabels script, then re-running MissingDataFX.sh... "
 	chmod u+x ../shell/fetchTaxonLabels.sh
 	../shell/fetchTaxonLabels.sh
 
-	rm ./missingDataFXTester.Rout ./MissingDataFX_R_Workspace.RData ./*.pdf ./*_test_output.txt ./*_pvalues.txt
+	if [[ -n $(find . -name "missingDataFXTester.Rout" -type f) ]]; then rm ./missingDataFXTester.Rout; fi
+	if [[ -n $(find . -name "MissingDataFX_R_Workspace.RData" -type f) ]]; then rm ./MissingDataFX_R_Workspace.RData; fi
+	if [[ -n $(find . -name "*.pdf" -type f) ]]; then rm ./*.pdf; fi
+	if [[ -n $(find . -name "*_test_output.txt" -type f) ]]; then rm ./*_test_output.txt; fi 
+	if [[ -n $(find . -name "*_pvalues.txt" -type f) ]]; then rm ./*_pvalues.txt; fi
 
 
 echo "
