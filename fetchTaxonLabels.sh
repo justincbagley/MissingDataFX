696752f2093d61c01deba11ca2f3b5a27212db18
Justin C. Bagley <justincbagley@users.noreply.github.com>
2017-03-15 12:08:24 -0500

    Update fetchTaxonLabels.sh

--- fetchTaxonLabels.sh
+++ fetchTaxonLabels.sh
@@ -5,10 +5,10 @@
 ###### STEP #1: SETUP AND USER INPUT.
 ###### Set paths and filetypes as different environmental variables:
 	MY_PATH=`pwd -P`				## This script assumes it is being run in a sub-folder
-									## of the MissingDataFX master directory specific to the 
-									## current analysis. 
-echo "INFO      | $(date) |          Setting working directory to dir: "
-echo "INFO      | $(date) |          $MY_PATH "
+							## of the MissingDataFX master directory specific to the 
+							## current analysis. 
+## echo "INFO      | $(date) |          Setting working directory to dir: "
+## echo "INFO      | $(date) |          $MY_PATH "
 	CR=$(printf '\r');
 	calc () {
 	bc -l <<< "$@" 
@@ -21,7 +21,6 @@ echo "INFO      | $(date) |          Reading in input NEXUS file(s)... "
 
 ###### STEP #2: MAKE AND RUN SCRIPT TO FETCH TAXON LABELS AND SAVE TO FILE.
 		echo "INFO      | $(date) |          Running fetchTaxonLabels script... "
-
 		fetchTaxonLabels () {
 
 		(
@@ -34,7 +33,9 @@ echo "INFO      | $(date) |          Reading in input NEXUS file(s)... "
 				MY_NUMLINES_END_1ST_BLOCK="$(grep -n "^$" ./"${MY_NEXUS_BASENAME}"_headless.nex | head -n1 | sed 's/\://g; s/\ //g')"
 				MY_NUM_TAX="$(calc "$MY_NUMLINES_END_1ST_BLOCK"-1)"
 				head -n"$MY_NUM_TAX" ./"${MY_NEXUS_BASENAME}"_headless.nex > ./"${MY_NEXUS_BASENAME}"_headless_seqData.txt
-				perl -pe 's/^[\ ]*//g; s/\ +.*$//g' ./"${MY_NEXUS_BASENAME}"_headless_seqData.txt > ./"${MY_NEXUS_BASENAME}"_taxonLabels.txt
+
+					echo "INFO      | $(date) |          Making a taxonLabels file for taxa in "$i" "
+					perl -pe 's/^[\ ]*//g; s/\ +.*$//g' ./"${MY_NEXUS_BASENAME}"_headless_seqData.txt > ./"${MY_NEXUS_BASENAME}"_taxonLabels.txt
 
 				rm ./"${MY_NEXUS_BASENAME}"_headless.nex ./"${MY_NEXUS_BASENAME}"_headless_seqData.txt
 			done
