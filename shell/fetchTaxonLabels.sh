8104ebab8f51342acaf86795b4bbc89edf67cd46
Justin C. Bagley <justincbagley@users.noreply.github.com>
2017-03-15 12:11:46 -0500

    Update fetchTaxonLabels.sh

--- shell/fetchTaxonLabels.sh
+++ shell/fetchTaxonLabels.sh
@@ -2,25 +2,26 @@
 
 ################################## fetchTaxonLabels.sh ###################################
 
+echo "INFO      | $(date) |          Running fetchTaxonLabels script... "
+
 ###### STEP #1: SETUP AND USER INPUT.
 ###### Set paths and filetypes as different environmental variables:
 	MY_PATH=`pwd -P`				## This script assumes it is being run in a sub-folder
 							## of the MissingDataFX master directory specific to the 
 							## current analysis. 
-## echo "INFO      | $(date) |          Setting working directory to dir: "
-## echo "INFO      | $(date) |          $MY_PATH "
+echo "INFO      | $(date) |               Setting working directory to dir: "
+echo "INFO      | $(date) |               $MY_PATH "
 	CR=$(printf '\r');
 	calc () {
 	bc -l <<< "$@" 
 }
 
-echo "INFO      | $(date) |          Reading in input NEXUS file(s)... "
+echo "INFO      | $(date) |               Reading in input NEXUS file(s)... "
 ###### Read in the NEXUS file(s), by getting filenames matching .nex or .NEX patterns. 
 	MY_NEXUS_FILES="$(find . \( -name "*.nex" -o -name "*.NEX" -o -name "*.Nex" \) -type f | sed 's/\.\///g')"
 
 
 ###### STEP #2: MAKE AND RUN SCRIPT TO FETCH TAXON LABELS AND SAVE TO FILE.
-		echo "INFO      | $(date) |          Running fetchTaxonLabels script... "
 		fetchTaxonLabels () {
 
 		(
@@ -34,7 +35,7 @@ echo "INFO      | $(date) |          Reading in input NEXUS file(s)... "
 				MY_NUM_TAX="$(calc "$MY_NUMLINES_END_1ST_BLOCK"-1)"
 				head -n"$MY_NUM_TAX" ./"${MY_NEXUS_BASENAME}"_headless.nex > ./"${MY_NEXUS_BASENAME}"_headless_seqData.txt
 
-					echo "INFO      | $(date) |          Making a taxonLabels file for taxa in "$i" "
+					echo "INFO      | $(date) |               Making a taxonLabels file for taxa in "$i" "
 					perl -pe 's/^[\ ]*//g; s/\ +.*$//g' ./"${MY_NEXUS_BASENAME}"_headless_seqData.txt > ./"${MY_NEXUS_BASENAME}"_taxonLabels.txt
 
 				rm ./"${MY_NEXUS_BASENAME}"_headless.nex ./"${MY_NEXUS_BASENAME}"_headless_seqData.txt
